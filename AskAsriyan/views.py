77416519526e0bb4b582320456c9218763d2e8db
Ed Asriyan <ed-asriyan@protonmail.com>
2016-11-05 20:38:55 +0300

    Create base view

--- AskAsriyan/views.py
+++ AskAsriyan/views.py
@@ -1,4 +1,12 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render_to_response
+import random
 
+def base_decorator(func):
+    def decorator(request, *args, **kwargs):
+        tags = [''.join([chr(random.randint(ord('A'), ord('z'))) for j in range(0, random.randint(5, 10))]) for i in
+                range(random.randint(15, 35))]
+        print(tags)
+        return func(request, kwargs, tags=tags)
 
+    return decorator
