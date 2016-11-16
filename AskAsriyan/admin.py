13accd410a3f490ec78c90c0cdcb52fa055cefe1
Ed Asriyan <ed-asriyan@protonmail.com>
2016-11-17 02:30:27 +0300

    Create Comment model

--- AskAsriyan/admin.py
+++ AskAsriyan/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Article
+from . import models
 
-admin.site.register(Article)
+admin.site.register(models.Article)
+admin.site.register(models.Comment)
--- AskAsriyan/models.py
+++ AskAsriyan/models.py
@@ -12,5 +12,23 @@ class Article(models.Model):
     article_rating = models.IntegerField()
     article_author = models.ForeignKey(User)
 
+    def get_comments(self):
+        return Comment.objects.filter(comment_article_id=self.id)
+
     def get_url(self):
         return '/article{article_id}'.format(article_id=self.id)
+
+
+class Comment(models.Model):
+    class Meta:
+        db_table = "comment"
+
+    comment_author = models.ForeignKey(User)
+    comment_body = models.TextField()
+    comment_date = models.DateField()
+    comment_is_correct = models.BooleanField(default=False)
+    comment_rating = models.IntegerField(default=0)
+    comment_article = models.ForeignKey('Article')
+
+    def get_url(self):
+        return self.comment_article.get_url()
